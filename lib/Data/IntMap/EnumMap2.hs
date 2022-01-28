ea213fae283c40661b6dd852335afe40e139f33c
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-01-28 14:50:00 +0100

    haskell/core-libraries-committee#33 : Remove uses of MonadFail (ST _)
    There was a use of `fail` for `Maybe` which is entirely pointless.

--- lib/Data/IntMap/EnumMap2.hs
+++ lib/Data/IntMap/EnumMap2.hs
@@ -50,7 +50,7 @@ notMember k (EnumMap m) = M.notMember (fromEnum k) m
 
 {-# INLINE lookup #-}
 lookup :: (Enum key) => key -> EnumMap key a -> Maybe a
-lookup k (EnumMap m) = maybe (fail "EnumMap.lookup failed") return $ M.lookup (fromEnum k) m
+lookup k (EnumMap m) = M.lookup (fromEnum k) m
 
 findWithDefault :: (Enum key) => a -> key -> EnumMap key a -> a
 findWithDefault a k (EnumMap m) = M.findWithDefault a (fromEnum k) m
